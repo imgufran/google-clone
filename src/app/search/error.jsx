24de8557b312793ded1cb3ae9fee1f46bc7de6b0
Gufran <iamgufran@outlook.com>
2023-09-04 11:04:38 +0530

    added error page to handle errors

---
+++ src/app/search/error.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({error, reset}) {
+  useEffect(() => {
+    console.log("Error: ", error.message);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col justify-center items-center p-10">
+      <h1 className="text-3xl mb-4">Something went wrong</h1>
+      <button className="text-blue-500" onClick={() => reset()}>Try again</button>
+    </div>
+  )
+}
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,12 +1,32 @@
-export default async function WebpageSearchPage({searchParams}) {
+export default async function WebpageSearchPage({ searchParams }) {
   const res = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.search}`
   );
 
+  if (!res.ok) {
+    throw new Error("Something went wrong");
+  }
+
   const data = await res.json();
   const results = data.items;
 
-  return <>
-    {results && results.map((result) => <h1>{result.title}</h1>)}
-  </>;
+  if (!results) {
+    return (
+      <div className="ml-10">
+        <p className="my-3">
+          Your search -{" "}
+          <span className="font-semibold">{searchParams.search}</span> - did not
+          match any documents.
+        </p>
+        <p className="my-3">Suggestions:</p>
+        <ul className="my-3 list-disc">
+          <li>Make sure that all words are spelled correctly.</li>
+          <li>Try different keywords.</li>
+          <li>Try more general keywords.</li>
+        </ul>
+      </div>
+    );
+  }
+
+  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
 }
