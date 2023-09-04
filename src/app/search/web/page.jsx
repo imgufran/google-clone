9eea15602e06af308000cc757c1dd86b71d97426
Gufran <iamgufran@outlook.com>
2023-09-04 10:19:49 +0530

    implemented the functionality to fetch data from google search api

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,5 +1,12 @@
-export default function WebpageSearchPage() {
-  return (
-    <div>WebpageSearchPage</div>
-  )
+export default async function WebpageSearchPage({searchParams}) {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.search}`
+  );
+
+  const data = await res.json();
+  const results = data.items;
+
+  return <>
+    {results && results.map((result) => <h1>{result.title}</h1>)}
+  </>;
 }
